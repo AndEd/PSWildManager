41321b96050c62fbafb85ec1da14708b8df53583
AndEd <zx3211226@gmail.com>
2017-11-22 23:33:00 +0900

    Update PSWildManager.php

--- src/pswild/PSWildManager.php
+++ src/pswild/PSWildManager.php
@@ -12,57 +12,83 @@ use pocketmine\level\generator\Generator;
 use pocketmine\level\generator\normal\Normal;
 
 class PSWildManager extends PluginBase implements Listener{
-    private $normal = [];
-    private $wild = [];
-    private $wildlast = [];
+    private $setting = [], $playerData = [];
     public function onEnable(){
+        @mkdir($this->getDataFolder());
+        @mkdir($this->getServer()->getDataPath() . "/worlds/wild");
+        
+        $this->setting = $this->load($this->getDataFolder()."setting.json", [
+            "wild-level" => ["wild"]
+        ]);
+        
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
-        Generator::addGenerator(Normal::class, "wild");
-        $gener = Generator::getGenerator("wild");
         if(!($this->getServer()->loadLevel("wild"))){
-            @mkdir($this->getServer()->getDataPath() . "/" . "worlds" . "/" . "wild");
-            $options = [];
-            $this->getServer()->generateLevel("wild", 0, $gener, $options);
+            Generator::addGenerator(Normal::class, "wild");
+            $gener = Generator::getGenerator("wild");
+            $this->getServer()->generateLevel("wild", 0, $gener, []);
             $this->getLogger()->info("야생 생성 완료.");
         }
         $this->getLogger()->info("야생 로드 완료.");
     }
-    public function onCommand(CommandSender $pl, Command $command, string $label, array $args) : bool{
-        if($command->getName() !== "야생") return true;
-        if(! $pl instanceof Player) return true;
-        if($pl->getLevel()->getName() == "wild"){
-        if(! isset($this->normal[$pl->getName()])){$this->normal[$pl->getName()] = [];
+    
+    public function load($path, array $defaultArray) {
+        return (new Config($path, Config::JSON, $defaultArray))->getAll();
+    }
+    
+    public function save($path, array $data, $async = true) {
+        $conf = new Config($path, Config::JSON, []);
+        $conf->setAll($data);
+        $conf->save($async);
+    }
+    
+    public function setInventoryData(Player $player, array $data) {
+        $player->getInventory()->clearAll();
+        foreach($i = 0; $i < $size; $i ++) {
+            $item->setItem($i, $this->toItem($arr["inven"][$i]));
         }
-        for ($i = 1; $i >= $pl->getInventory()->getSize(); $i++) {
-            $item = $pl->getInventory()->getItem($i);
-            if($item->getId() == 0) continue;
-            $this->wild[$pl->getName()] [$item->getId()] = (! isset($this->wild[$pl->getName()] [$item->getId()]))? 'id => '.$item->getId().','.'dam => '.$item->getDamage().','.'count => '.$this->wild[$pl->getName()] [$item->getId()] + $item->count : 'id => '.$item->getId().','.'dam => '.$item->getDamage().','.'count => '.$item->count;
+        foreach($i = 0; $i < 4; $i ++) {
+            $now = $i - $size;
+            $item->setArmorItem($now, $this->toItem($arr["armor"][$now]));
         }
-        $pl->getInventory()->clearAll();
-        for ($i = 0; $i >= count($this->normal[$pl->getName()]); $i++) {
-            $pl->getInventory()->addItem(new Item($this->normal[$pl->getName()] [$i] ['id'], $this->normal[$pl->getName()] [$i]['dam'],$this->normal[$pl->getName()] [$i]['count']));
+    }
+    
+    public function getInventoryData(Player $player) {
+        $inventory = $player->getInventory();
+        $size = $inventory->getSize();
+        $arr = [];
+        foreach($i = 0; $i < $size; $i ++) {
+            $arr["inven"][$i] = $this->toData($inventory->getItem($i));
         }
-        }else{if(! isset($this->wild[$pl->getName()])){$this->wild[$pl->getName()] = [];}
-        for ($i = 1; $i >= $pl->getInventory()->getSize(); $i++) {
-            $item = $pl->getInventory()->getItem($i);
-            if($item->getId() == 0) continue;
-            $this->normal[$pl->getName()] [$i - 1] = (! isset($this->normal[$pl->getName()] [$item->getId()]))? 'id => '.$item->getId().','.'dam => '.$item->getDamage().','.'count => '.$this->normal[$pl->getName()] [$item->getId()] + $item->count : 'id => '.$item->getId().','.'dam => '.$item->getDamage().','.'count => '.$item->count;
+        foreach($i = 0; $i < 4; $i ++) {
+            $arr["armor"][$i - $size] = $this->toData($inventory->getArmorItem($i));
         }
-        $pl->getInventory()->clearAll();
-        for ($i = 0; $i >= count($this->wild[$pl->getName()]); $i++) {
-            $pl->getInventory()->addItem(new Item($this->wild[$pl->getName()] [$i] ['id'], $this->wild[$pl->getName()] [$i]['dam'],$this->wild[$pl->getName()] [$i]['count']));
+    }
+    
+    public function toData(Item $item) {
+        $arr = [];
+        if ($item->hasCustomName()) $arr["name"] = $item->getCustomName();
+        if (!empty($item->getLore())) $arr["lore"] = $item->getLore();
+        if ($item->hasEnchantments()) {
+            foreach($item->getEnchantments() as $ench) {
+                $arr["enchantment"][$ench->getId()] = $ench->getLevel();
+            }
         }
+        $arr["id"] = $item->getId();
+        $arr["meta"] = $item->getDamage();
+        $arr["count"] = $item->getCount();
+        return $arr;
+    }
+    
+    public function toItem(array $data) {
+        $item = Item::get($data["id"], $data["meta"], $data["count"]);
+        if (isset($data["name"])) $item->setCustomName($data["name"]);
+        if (isset($data["lore"])) $item->setLore($data["lore"]);
+        if (isset($data["enchantment"])) {
+            foreach($data["enchantment"] as $id=>$level) {
+                $item->addEnchantment(Enchantment::getEnchantment($id)->setLevel($level));
+            }
         }
-        $blname = ($pl->getLevel()->getName() == "wild")? "야생" : "서버";
-        $pl->sendMessage($blname."에서의 인벤이 백업이 되었으며 돌아올시에 다시 인벤이 복구 됩니다! 안심하고 플레이 하세요!");
-        $blname = ($pl->getLevel()->getName() == "wild")? "world" : "wild";
-        if($blname == "wild"){
-            $a = (isset($this->wildlast[$pl->getName()]))? new Vector3($this->wildlast[$pl->getName()][0], $this->wildlast[$pl->getName()][1], $this->wildlast[$pl->getName()][2]) : new Vector3(rand(-500, 500),rand(-500, 500),rand(-500, 500));
-        $pl->teleport($this->getServer()->getLevelByName($blname)->getSafeSpawn($a),0,0);
-        }else{
-            $this->wildlast[$pl->getName()] = [$pl->x, $pl->y, $pl->z];
-        }
-        return true;
+        return $item;
     }
 }
